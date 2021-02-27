7d3808239606c03782c129874543bc28d256b6f4
Enzo Gabriel <eggqsaraiva@gmail.com>
2021-02-27 02:54:20 -0300

    Criando o Contexto Countdown e migrando funções para lá

--- dontstopmoving-next/src/components/Profile.tsx
+++ dontstopmoving-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContexts';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/enzoggqs.png" alt="Enzo Gabriel" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Enzo Gabriel</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
