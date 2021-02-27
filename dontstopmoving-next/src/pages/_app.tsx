cdb47f886b706747f2a01a256f2d8cb9b7d63ea1
Enzo Gabriel <eggqsaraiva@gmail.com>
2021-02-27 20:13:51 -0300

    Fazendo a aplicação salvar os dados do usuário, como nível e número de desafios completos para quadno dar refresh os números não resetar

--- dontstopmoving-next/src/pages/_app.tsx
+++ dontstopmoving-next/src/pages/_app.tsx
@@ -5,10 +5,7 @@ import { ChallengesProvider } from '../contexts/ChallengesContexts'
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-
+    <Component {...pageProps} />
   )
 }
 
